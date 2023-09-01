a15a14757ce0fdfc5c427582675da1362271beda
ikedh <mcalderon@digitalhouse.com>
2023-09-01 20:30:26 -0300

    API - Post - Base

---
+++ js/clase-13.js
@@ -0,0 +1,48 @@
+/* -------------------------------------------------------------------------- */
+/*                [1] FUNCION: esperamos la carga de la ventana               */
+/* -------------------------------------------------------------------------- */
+window.addEventListener('load', function () {
+
+    const formulario = this.document.querySelector('form')
+
+    formulario.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+    postearComentario()
+    })
+
+
+})
+
+/* -------------------------------------------------------------------------- */
+/*                 [2] FUNCION: capturamos los datos del form                 */
+/* -------------------------------------------------------------------------- */
+
+    
+
+
+/* -------------------------------------------------------------------------- */
+/*                    [3] FUNCION: enviar(postear) a la API                   */
+/* -------------------------------------------------------------------------- */
+// Nos basamos en la documentacion de la API:
+// https://jsonplaceholder.typicode.com/guide/
+
+    
+
+
+
+    /* https://jsonplaceholder.typicode.com/posts */
+/* -------------------------------------------------------------------------- */
+/*                      [4] FUNCION: renderizar respuesta                     */
+/* -------------------------------------------------------------------------- */
+
+    
+
+
+/* -------------------------------------------------------------------------- */
+/*                               MESA DE TRABAJO                              */
+/* -------------------------------------------------------------------------- */
+// En este caso vamos a trabajar la conexion con APIS,
+// en el siguiente DOC vamos a poder ver nuestra tarea
+// 👇
+// https://docs.google.com/document/d/1ZiCPf7IICvtp6rwfxoq5Wh5dJUROKqNw/preview
