294310deb4b5ce3f57e6b33e820c66f9aa549ef4
ikedh <mcalderon@digitalhouse.com>
2023-09-01 22:14:12 -0300

    POST configurado

--- js/clase-13.js
+++ js/clase-13.js
@@ -8,7 +8,7 @@ window.addEventListener('load', function () {
     formulario.addEventListener('submit', function (event) {
         event.preventDefault();
 
-    postearComentario()
+        postearComentario();
     })
 
 
@@ -17,9 +17,19 @@ window.addEventListener('load', function () {
 /* -------------------------------------------------------------------------- */
 /*                 [2] FUNCION: capturamos los datos del form                 */
 /* -------------------------------------------------------------------------- */
+function capturarDatos() {
+    const titulo = document.querySelector('#titulo');
+    const comentario = document.querySelector('#comentario');
 
-    
+    // armamos el objeto basado en lo que nos pide la api
+    let objeto = {
+        title: titulo.value,
+        body: comentario.value,
+        userId: 1,
+      }
 
+    return objeto;
+}
 
 /* -------------------------------------------------------------------------- */
 /*                    [3] FUNCION: enviar(postear) a la API                   */
@@ -27,16 +37,48 @@ window.addEventListener('load', function () {
 // Nos basamos en la documentacion de la API:
 // https://jsonplaceholder.typicode.com/guide/
 
-    
+function postearComentario() {
+    // 👇 usamos nuestra funcion para capturar los datos y guardarlos como objeto
+    const datos = capturarDatos();
 
+    // 👇 armamos las configuraciones
+    // la api acepta JSON, por eso stringuificamos los datos
+    const settings = {
+        method: 'POST',
+        body: JSON.stringify(datos),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    }
 
+    fetch('https://jsonplaceholder.typicode.com/posts', settings)
+        .then((respuesta) => respuesta.json())
+        .then((data) => {
+            console.log(data);
+            renderizarRespuesta(data);
+        });
+}
 
-    /* https://jsonplaceholder.typicode.com/posts */
 /* -------------------------------------------------------------------------- */
 /*                      [4] FUNCION: renderizar respuesta                     */
 /* -------------------------------------------------------------------------- */
 
+function renderizarRespuesta(datos) {
+    const div = document.querySelector('.respuesta')
     
+    const template = `
+        <p>✅ Datos cargados en el servidor</p>
+        <p>
+            Title: ${datos.title}
+        </p>
+        <p>
+            Body: ${datos.body}
+        </p>
+    `;
+
+    div.innerHTML = template;
+
+}
 
 
 /* -------------------------------------------------------------------------- */
